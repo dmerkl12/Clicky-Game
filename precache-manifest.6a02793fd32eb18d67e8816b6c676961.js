f834cf00bef2ee81d1f12bbb945acb84fa9e2feb
Daniel Merkl <dan.merkl.90@gmail.com>
2019-10-15 23:46:39 -0500

    Updates

---
+++ precache-manifest.6a02793fd32eb18d67e8816b6c676961.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9794a842cd47bb6a507b9ccb6a08cd4",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "28e33a70718bbfbcbbd2",
+    "url": "/Clicky-Game/static/css/main.cffd80c8.chunk.css"
+  },
+  {
+    "revision": "4cafa49515aee7290950",
+    "url": "/Clicky-Game/static/js/2.ff9cd7af.chunk.js"
+  },
+  {
+    "revision": "28e33a70718bbfbcbbd2",
+    "url": "/Clicky-Game/static/js/main.76408e7f.chunk.js"
+  },
+  {
+    "revision": "eabcff367a27d49b89d0",
+    "url": "/Clicky-Game/static/js/runtime-main.ca802268.js"
+  }
+]);
